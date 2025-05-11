875ea570598c425b94ef0806d7757fa4078710c7
Apolo <paolacrisnvs@gmail.com>
2025-05-11 09:10:41 -0300

    Add project files.

---
+++ task4-readline/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Digite uma nota: ");
+double nota = Convert.ToInt16(Console.ReadLine());
+
+if (nota >= 7)
+{
+    Console.WriteLine("Aprovado!");
+}
+else
+{
+    Console.WriteLine("Reprovado");
+}
